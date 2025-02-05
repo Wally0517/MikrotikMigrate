32209b53902b3eb1b5f7841841de64d8ba9dce11
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-02-04 19:31:08 -0600

    Update Mikrotikconvert.py

--- Mikrotikconvert.py
+++ Mikrotikconvert.py
@@ -363,5 +363,5 @@ def upload_file():
 
     return send_file(processed_file_path, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
